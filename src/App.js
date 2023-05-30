fdc4dfcf68a87a1f5a0fa5ac8a46ea49f8b9e2b5
Delnia Alipour <delniaalipur@gmail.com>
2023-05-30 21:43:49 +0100

    table created
    descending order of scores

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import allCountryScores from "./scores";
+import "./App.css";
+import React from "react";
 
-function App() {
+const HighScoreTable = ({ country }) => {
+  const sortedScores = country.scores.sort((a, b) => b.s - a.s);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="HighScoreTable">
+      <h3>HIGH SCORES: {country.name}</h3>
+      <table>
+        <tbody>
+          {sortedScores.map((score, index) => (
+            <tr key={index}>
+              <td>{score.n}</td>
+              <td>{score.s}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>High Scores Per Country</h2>
+      {allCountryScores.map((country, index) => (
+        <HighScoreTable key={index} country={country} />
+      ))}
+    </div>
+  );
+};
 
 export default App;
